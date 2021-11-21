a424b5ee87a8e201c7cb04ec801342842b04a700
ShawqySchenkels <r0831086@student.thomasmore.be>
2021-11-21 19:52:49 +0100

    updates

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,15 @@ export default function Home() {
         <p className="description">
            <code>Made by Shawqy Schenkels</code>
         </p>
+
+        <h1>Some screenshots</h1>
+      <img style="width: 300px;" src="scr1.PNG"></img>
+      <img style="width: 300px;" src="scr2.PNG"></img>
+      <img style="width: 300px;" src="scr3.PNG"></img>
+      <img style="width: 300px;" src="scr4.PNG"></img>
+      <p>The app u see, is a quiz app. Here the user can press the start button, then he gets a question with a couple of possible answers. After the user answered the question he gets the next question and so forth until he answered all the questions. After all the questions are answered u get the results shown on the screen. U have 15 seconds per answer</p>
+      <img style="width: 300px; height: 300px;" src="frame.png"></img>
       </main>
-	  <img src="frame.png"></img>
 
       <Footer />
     </div>
